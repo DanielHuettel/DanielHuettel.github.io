ecd10d94eec615a216f71fb7c25ddd0c40194d94
DHuettel <48473142+DHuettel@users.noreply.github.com>
2019-03-20 18:05:47 +0100

    map

---
+++ nz/main.js
@@ -0,0 +1,9 @@
+/*alert("hallo welt")*/
+
+const div = document.getElementById("map");
+const lat = div.getAttribute("data-lat");
+const lng = div.getAttribute("data-lng");
+const title = div.getAttribute("data-title");
+
+//console.log("Breite="lat,"Länge=",lng,"Titel=",title);
+
