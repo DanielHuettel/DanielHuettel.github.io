00e1141166f013520f548819a3eac3ca0e8102a9
DHuettel <48473142+DHuettel@users.noreply.github.com>
2019-03-24 16:24:24 +0100

    7/10 variables

--- workload 3/main2.js
+++ workload 3/main2.js
@@ -1,6 +1,2 @@
-let gainedDollar = 3;
-let lostDollar = 50;
-gainedDollar++;
-lostDollar--;
-console.log(gainedDollar)
-console.log(lostDollar)
+let favoriteAnimal = 'Lurch'
+console.log('My favorite animal: ' + favoriteAnimal)
