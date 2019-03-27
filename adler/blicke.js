acdabcc15e5c2fa333d8d3f1dcbd30db4ff5716e
Hüttel <csaw3093@uibk.ac.at>
2019-03-27 18:35:03 +0100

    pins hinzugefügt mit Excel Spreadsheet to Array Converter

--- adler/blicke.js
+++ adler/blicke.js
@@ -1,23 +1,102 @@
 const adlerblicke = [{
-        kunde: "Wilder Kaiser",
-        standort: "Gruttenhütte",
-        seehoehe: 1640,
-        lat: 47.55564,
-        lng: 12.31861,
-    },
 
-    {
-        kunde: "Bergbahn Scheffau",
-        standort: "Brandstadl",
-        seehoehe: 1640,
-        lat: 47.4912,
-        lng: 12.248,
+
+        "Wilder Kaiser ": "Bergbahn Scheffau ",
+        "Gruttenhütte": "Brandstadl",
+        "1640": 1640,
+        "47.55564": 47.49120,
+        "12.31861": 12.24800
     },
     {
-        kunde: "Lechtal Tourismus",
-        standort: "Sonnalm Jöchelspitze",
-        seehoehe: 1786,
-        lat: 47.41028,
-        lng: 10.60083,
+        "Wilder Kaiser ": "Lechtal Tourismus ",
+        "Gruttenhütte": "Sonnalm Jöchelspitze",
+        "1640": 1786,
+        "47.55564": 47.27528,
+        "12.31861": 10.36505
     },
+    {
+        "Wilder Kaiser ": "TVB Region Hall-Wattens ",
+        "Gruttenhütte": "Kugelwald am Glungezer",
+        "1640": 1541,
+        "47.55564": 47.40785,
+        "12.31861": 11.91222
+    },
+    {
+        "Wilder Kaiser ": "TVB St. Anton am Arlberg ",
+        "Gruttenhütte": "Kapall",
+        "1640": 2330,
+        "47.55564": 47.14805,
+        "12.31861": 10.24917
+    },
+    {
+        "Wilder Kaiser ": "Tourismusverband Achensee",
+        "Gruttenhütte": "Am Rofan, neben dem Almstüberl, gegenüber der Rofanseilbahn Bergstation",
+        "1640": 1831,
+        "47.55564": 47.44154,
+        "12.31861": 11.76364
+    },
+    {
+        "Wilder Kaiser ": "Tourismusverband Achensee",
+        "Gruttenhütte": "Am Zwölferkopf, direkt beim Alpengasthaus Karwendel am Balkon",
+        "1640": 1491,
+        "47.55564": 47.42783,
+        "12.31861": 11.69493
+    },
+    {
+        "Wilder Kaiser ": "Ferienregion Hohe Salve  ",
+        "Gruttenhütte": "Buchacker Alm Adlerhorst",
+        "1640": 1460,
+        "47.55564": 47.53789,
+        "12.31861": 12.02317
+    },
+    {
+        "Wilder Kaiser ": "Alpbachtal",
+        "Gruttenhütte": "Berghaus Sonnwendjoch",
+        "1640": 1780,
+        "47.55564": 47.46408,
+        "12.31861": 11.82863
+    },
+    {
+        "Wilder Kaiser ": "TVB Imst",
+        "Gruttenhütte": "Muttekopf",
+        "1640": 2050,
+        "47.55564": 47.25566,
+        "12.31861": 10.67688
+    },
+    {
+        "Wilder Kaiser ": "TVB Kufstein",
+        "Gruttenhütte": "Brentenjoch",
+        "1640": 1226,
+        "47.55564": 47.57842,
+        "12.31861": 12.21217
+    },
+    {
+        "Wilder Kaiser ": "Nordkette Innsbruck ",
+        "Gruttenhütte": "Seegrube",
+        "1640": 1905,
+        "47.55564": 47.30623,
+        "12.31861": 11.37941
+    },
+    {
+        "Wilder Kaiser ": "Nordkette Innsbruck ",
+        "Gruttenhütte": "Hafelekar ",
+        "1640": 2269,
+        "47.55564": 47.31185,
+        "12.31861": 11.38376
+    },
+    {
+        "Wilder Kaiser ": "TVB Wilder Kaiser",
+        "Gruttenhütte": "Going Astberg Speicherteich 2",
+        "1640": 1250,
+        "47.55564": 47.50083,
+        "12.31861": 12.32417
+    },
+    {
+        "Wilder Kaiser ": "TVB Wilder Kaiser",
+        "Gruttenhütte": "Going Astberg Speicherteich 2",
+        "1640": 1250,
+        "47.55564": 47.50167,
+        "12.31861": 12.32306
+    }
+
 ];
