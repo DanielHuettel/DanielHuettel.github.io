8f31ccc96df5cd965f1f551d901df7695455d173
Hüttel <csaw3093@uibk.ac.at>
2019-03-27 17:01:06 +0100

    karte, pin  und popup hinzugefügt

--- adler/main.js
+++ adler/main.js
@@ -1,9 +1,12 @@
 /*alert("hallo welt")*/
 
 const div = document.getElementById("map");
-const breite = div.getAttribute("data-lat");
-const laenge = div.getAttribute("data-lng");
-const titel = div.getAttribute("data-title");
+const breite1 = div.getAttribute("data-lat1");
+const laenge1 = div.getAttribute("data-lng1");
+const titel1 = div.getAttribute("data-title1");
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
 
 console.log("Breite=",breite,"Länge=",laenge,"Titel=",titel);
 
@@ -21,9 +24,13 @@ karte.setView(
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 // Positionsmarker hinzufügen
-let pin = L.marker(
-    [breite,laenge]
+let pin1 = L.marker(
+    [breite1,laenge1]
+).addTo(karte)
+let pin2 = L.marker(
+    [breite2,laenge2]
 ).addTo(karte)
 
 // Popup zum PIN hängen
-pin.bindPopup(titel).openPopup();
+pin1.bindPopup(titel1).openPopup();
+pin2.bindPopup(titel2).openPopup();
